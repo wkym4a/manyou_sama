90e998949113a4ffee455b532179435baf282826
若山侑輝 <wkym_4a@icloud.com>
2019-04-27 13:02:47 +0900

    タイムゾーン設定をいったん無効化

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,14 @@ module ManyouSama
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+        # #タイムゾーン設定を東京に
+        # config.time_zone = 'Tokyo'
+        # config.active_record.default_timezone = :local
+        
     # サービスをオートロードする(サービスクラスの追加を試してみる)……本番環境では上でないとだめなはず
   config.eager_load_paths += %W( #{config.root}/extras )
 #    config.autoload_paths += %W(#{config.root}/app/services)
 
-    #タイムゾーン設定を東京に
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
 
     #  #「jsが二回目に動かない」への対処法として、 この行を追加
     # config.action_view.automatically_disable_submit_tag = false
