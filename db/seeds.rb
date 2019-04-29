3ce327c0fd9e9ad5c234bc6dd96d6da7924cbb4e
若山侑輝 <wkym_4a@icloud.com>
2019-04-29 19:53:31 +0900

    ステップ22_修正_Admin権限設定_権限追加に伴い、「いったん全員を管理者にする」ためのseed文を記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-first_user = User.new
-first_user.cd = "000"
-first_user.name = "管理用初期ユーザー"
-first_user.email = "kanri@test.co.jp"
-first_user.password = "password"
-first_user.password_confirmation = "password"
+#↓管理者権限追加に伴い、いったん全員を管理者にするseed
+User.update_all(admin_status: 9)
 
-first_user.save
-
-tasks_first_user = Task.where(" user_id is null ")
-tasks_first_user.update(user_id: first_user.id)
+# ######↓初回実施用#####
+# first_user = User.new
+# first_user.cd = "000"
+# first_user.name = "管理用初期ユーザー"
+# first_user.email = "kanri@test.co.jp"
+# first_user.password = "password"
+# first_user.password_confirmation = "password"
+#
+# first_user.save
+#
+# tasks_first_user = Task.where(" user_id is null ")
+# tasks_first_user.update(user_id: first_user.id)
+# ######↑初回実施用#####
